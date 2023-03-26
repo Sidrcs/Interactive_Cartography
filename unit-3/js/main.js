9d19f8d1beac85e36b207e2880c3c46703ae6d3f
Sid-rcs <91824047+Sidrcs@users.noreply.github.com>
2023-03-26 14:27:43 -0500

    D3 - Lesson 1
    Create a visible SVG graphic with an inner rectangle using properly formatted D3 code blocks in the main.js script of your unit-3 site.

--- unit-3/js/main.js
+++ unit-3/js/main.js
@@ -1,5 +1,28 @@
 window.onload = function(){
-    var container = d3.select("body")
-        .append("svg")
+    var w = 900, h = 500;
+
+    var container = d3.select("body") // Selecting body from DOM
+        .append("svg") //adding an SVG element
+        .attr("width", w) //assig width
+        .attr("height",h) //assign height
+        .attr("class", "container") //assign a class name
+        .style("background-color","rgba(0,0,0,0.2)"); //ending the block
+
+    //inner rect
+    var innerRect = container.append("rect") //add <rect> element
+            .datum(400)
+            .attr("width",function(d){
+                return d*2
+            }) //assign width
+            .attr("height",function(d){
+                return d
+            })//assign height
+            .attr("class", "innerRect")
+            .attr("x",50)
+            .attr("y",50)
+            .style("fill","#FFFFFF")
+
+
     console.log(container)
+    console.log(innerRect)
 }
