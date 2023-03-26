30c40a9d2a25678e430b06b316c571bceb6b82d7
Sid-rcs <91824047+Sidrcs@users.noreply.github.com>
2023-03-26 15:27:36 -0500

    D3 Lesson 3
    Using properly formatted D3 code blocks, create a bubble chart of the Week 2 example city data with circles named according to city name and sized according to city population.

--- unit-3/js/main.js
+++ unit-3/js/main.js
@@ -22,7 +22,8 @@ window.onload = function(){
             .attr("y",50)
             .style("fill","#FFFFFF")
 
-    //Example 2.5 line 1
+    //Example
+    /*
     var dataArray = [10, 20, 30, 40, 50];
 
     var circles = container.selectAll(".circles") //but wait--there are no circles yet!
@@ -42,5 +43,47 @@ window.onload = function(){
         });
 
     console.log(container)
-    console.log(innerRect)
+    console.log(innerRect)*/
+
+    var cityPop = [
+        { 
+            city: 'Madison',
+            population: 233209
+        },
+        {
+            city: 'Milwaukee',
+            population: 594833
+        },
+        {
+            city: 'Green Bay',
+            population: 104057
+        },
+        {
+            city: 'Superior',
+            population: 27244
+        }
+    ];
+
+    //Example
+    var circles = container.selectAll(".circles") //create an empty selection
+        .data(cityPop) //here we feed in an array
+        .enter() //one of the great mysteries of the universe
+        .append("circle") //inspect the HTML--holy crap, there's some circles there
+        .attr("class", "circles")
+        .attr("id", function(d){
+            return d.city;
+        })
+        .attr("r", function(d){
+            //calculate the radius based on population value as circle area
+            var area = d.population * 0.01;
+            return Math.sqrt(area/Math.PI);
+        })
+        .attr("cx", function(d, i){
+            //use the index to place each circle horizontally
+            return 90 + (i * 180);
+        })
+        .attr("cy", function(d){
+            //subtract value from 450 to "grow" circles up from the bottom instead of down from the top of the SVG
+            return 450 - (d.population * 0.0005);
+        });
 }
