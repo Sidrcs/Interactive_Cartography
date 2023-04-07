99db2c9a35d8ef46361cd905bf2c373a3818228d
Sid-rcs <91824047+Sidrcs@users.noreply.github.com>
2023-04-07 00:27:48 -0500

    week 9: update comments
    updated comments in main.js

--- unit-3/js/main.js
+++ unit-3/js/main.js
@@ -1,16 +1,21 @@
 window.onload = setMap();
 
 function setMap(){
-
+    // d3.csv(), d3.json() methods read csv, topojson files
+    // promises container is created to hold the promise
     var promises = [d3.csv("data/D3_Lab_Wisc_Counties.csv"),
         d3.json("data/Wisc_counties.topojson")]
-    
+    // helps to load the data asynchronously
+    // bind the output into callback function
     Promise.all(promises).then(callback);
 
+    // callback reads the output response of promises (read files - csv, topojson)
+    // retrieves the file information
     function callback(data){
         csvData = data[0];
         wisconsin = data[1];
 
+        // testing whether the files are loaded correctly or not
         console.log(csvData);
         console.log(wisconsin);
     };
