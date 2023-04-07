f30fe0269049862ee13934ca6456b67e519e99a1
Sid-rcs <91824047+Sidrcs@users.noreply.github.com>
2023-04-07 00:32:26 -0500

    week 9: callback function update
    Loaded datasets using a JavaScript promise and confirm the data are loading correctly using the console.

--- unit-3/js/main.js
+++ unit-3/js/main.js
@@ -4,7 +4,7 @@ function setMap(){
     // d3.csv(), d3.json() methods read csv, topojson files
     // promises container is created to hold the promise
     var promises = [d3.csv("data/D3_Lab_Wisc_Counties.csv"),
-        d3.json("data/Wisc_counties.topojson")]
+        d3.json("data/Wisc_counties.topojson")];
     // helps to load the data asynchronously
     // bind the output into callback function
     Promise.all(promises).then(callback);
