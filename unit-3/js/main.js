b3e8fa2b27a30855f5249eff70cf5f6d1ca56758
Sid-rcs <91824047+Sidrcs@users.noreply.github.com>
2023-04-07 13:49:43 -0500

    Week 9: Updated Wisconsin Map Final
    Added graticule but that does not look well on the map.

--- unit-3/js/main.js
+++ unit-3/js/main.js
@@ -2,8 +2,8 @@ window.onload = setMap();
 
 function setMap(){
     //map frame dimensions
-    var width = 960,
-        height = 460;
+    var width = 900,
+        height = 500;
 
     //create new svg container for the map
     var map = d3.select("body")
@@ -15,7 +15,7 @@ function setMap(){
     //create Albers equal area conic projection centered on Wisconsin
     var projection = d3.geoAlbers()
         .center([0, 44.42])
-        .rotate([90, -1, 0])
+        .rotate([90, -0.5, 0])
         .parallels([42.5, 47.5])
         .scale(4500)
         .translate([width / 2, height / 2]);
@@ -44,6 +44,35 @@ function setMap(){
         // testing whether the files are loaded correctly or not
         console.log(csvData);
         console.log(wisconsin);
+
+        /*
+        //create graticule generator
+        var graticule = d3.geoGraticule()
+            .step([2, 1]); //place graticule lines every 5 degrees of longitude and latitude
+        
+        //create graticule background
+        var gratBackground = map.append("path")
+            //bind graticule background
+            .datum(graticule.outline()) 
+            //assign class for styling
+            .attr("class", "gratBackground") 
+            //project graticule
+            .attr("d", path) 
+
+        //create graticule lines
+        //select graticule elements that will be created
+        var gratLines = map.selectAll(".gratLines") 
+            //bind graticule lines to each element to be created
+            .data(graticule.lines()) 
+            //create an element for each datum
+            .enter() 
+            //append each element to the svg as a path element
+            .append("path") 
+            //assign class for styling
+            .attr("class", "gratLines") 
+            //project graticule lines
+            .attr("d", path); 
+        */
 
         // translate Wisconsin counties from topojson to geojson
         var wisconsinCounties = topojson.feature(wisconsin, wisconsin.objects.Wisc_counties).features;
