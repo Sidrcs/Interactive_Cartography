781a5f096e78dc2a1cd8fbb49c57f61722678789
Sid-rcs <91824047+Sidrcs@users.noreply.github.com>
2023-04-07 13:28:02 -0500

    Added d3.projection(), d3.geoPath()
    Create a path generator and use it to draw your background geometry and enumeration units in the browser. Style your background geometry in style.css.

--- unit-3/js/main.js
+++ unit-3/js/main.js
@@ -1,10 +1,36 @@
 window.onload = setMap();
 
 function setMap(){
-    // d3.csv(), d3.json() methods read csv, topojson files
+    //map frame dimensions
+    var width = 960,
+        height = 460;
+
+    //create new svg container for the map
+    var map = d3.select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
+
+    //create Albers equal area conic projection centered on Wisconsin
+    var projection = d3.geoAlbers()
+        .center([0, 44.42])
+        .rotate([90, -1, 0])
+        .parallels([42.5, 47.5])
+        .scale(4500)
+        .translate([width / 2, height / 2]);
+
+    var path = d3.geoPath()
+        .projection(projection);
+
+   
     // promises container is created to hold the promise
-    var promises = [d3.csv("data/D3_Lab_Wisc_Counties.csv"),
-        d3.json("data/Wisc_counties.topojson")];
+    var promises = [];
+
+    // d3.csv(), d3.json() methods read csv, topojson files
+    promises.push(d3.csv("data/D3_Lab_Wisc_Counties.csv"));
+    promises.push(d3.json("data/Wisc_counties.topojson"));
+
     // helps to load the data asynchronously
     // bind the output into callback function
     Promise.all(promises).then(callback);
@@ -20,7 +46,18 @@ function setMap(){
         console.log(wisconsin);
 
         // translate Wisconsin counties from topojson to geojson
-        var wisconsinCounties = topojson.feature(wisconsin, wisconsin.objects.Wisc_counties);
+        var wisconsinCounties = topojson.feature(wisconsin, wisconsin.objects.Wisc_counties).features;
+
+        //add Wisconsin to map
+        var state = map.selectAll(".state")
+            .data(wisconsinCounties)
+            .enter()
+            .append("path")
+            .attr("class", function(d){
+                return "counties " + d.properties.NAMELSAD;
+            })
+            .attr("d", path);
+
         // To check the conversion result 
         console.log(wisconsinCounties);
 
