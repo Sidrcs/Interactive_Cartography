815aa63a45541eaebd34764fc99889aa7809043e
Sid-rcs <91824047+Sidrcs@users.noreply.github.com>
2023-04-11 21:28:26 -0500

    Activity 10: lollipopchart.js
    Solved lollipop chart problem. Circles and lines are added and styled. Each circle is filled as per the color scheme.

--- unit-3/js/main.js
+++ unit-3/js/main.js
@@ -1,9 +1,11 @@
 (function(){
 
 //pseudo-global variable    
-var attrArray = ["stfid",	"NAMELSAD",	"hh1620_est",	"persons1620_est",	"hhkids1620_est",	"kids1620_est",	"seniors1620_est",	"edlesshs1620_est",	"langeng1620_est",	"langasn1620_est",	"racewhite1620_est",	"raceaa1620_est",	"raceasian1620_est",	"raceamind1620_est",	"hispanic1620_est",	"noncitizens1620_est",	"drive1620_est",	"pratesaipe20",	"pratekidssaipe20",	"prateacs1620_est",	"pratekidsacs1620_est",	"gini1620_est",	"renters1620_est",	"noveh1620_est"];
+var attrArray = ["stfid",	"NAMELSAD",	"hh1620_est",	"persons1620_est",	"hhkids1620_est",	"kids1620_est",	"seniors1620_est",	"edlesshs1620_est",	"langeng1620_est",	"langasn1620_est",	"racewhite1620_est",	"raceaa1620_est",	"raceasian1620_est",	"raceamind1620_est",	"hispanic1620_est",	"noncitizens1620_est",	"drive1620_est", "prateacs1620_est",	"pratekidsacs1620_est",	"gini1620_est",	"renters1620_est",	"noveh1620_est"];
 
-var expressed = attrArray[0]; //initial attribute
+var arrayDict = {"stfid" : "Unique ID",	"NAMELSAD" : "County name",	"hh1620_est":"Households",	"persons1620_est":"Total persons",	"hhkids1620_est":"Household with children",	"kids1620_est":"Children",	"seniors1620_est":"Seniors",	"edlesshs1620_est":"Education less than high school",	"langeng1620_est":"English spoken at home",	"langasn1620_est":"Asian language spoken at home",	"racewhite1620_est":"White",	"raceaa1620_est":"African American",	"raceasian1620_est":"Asian",	"raceamind1620_est":"Native Americans",	"hispanic1620_est":"Hispanic",	"noncitizens1620_est":"Non-citizens",	"drive1620_est":"Workers driving or carpooling to work", "prateacs1620_est":"Poverty (all persons)",	"pratekidsacs1620_est":"Poverty(kids)",	"gini1620_est":"Gini index of income inequality",	"renters1620_est":"Households renting home", "noveh1620_est":"Households without vehicle"};
+
+var expressed = attrArray[19]; // loaded attribute based on index
 
 window.onload = setMap();
 
@@ -11,7 +13,7 @@ window.onload = setMap();
 function setMap(){
     // map frame dimensions
     var width = window.innerWidth * 0.5,
-        height = 500;
+        height = 473;
 
     // create new svg container for the map
     var map = d3.select("body")
@@ -28,10 +30,10 @@ function setMap(){
         .scale(4500)
         .translate([width / 2, height / 2]);
 
+    // geopath() method helps in drawing the geometries
     var path = d3.geoPath()
-        .projection(projection);
+        .projection(projection); // path holds projection and helps in rendering the projection
 
-   
     // promises container is created to hold the promise
     var promises = [];
 
@@ -39,7 +41,7 @@ function setMap(){
     promises.push(d3.csv("data/wisconsin_counties_data.csv"));
     promises.push(d3.json("data/Wisc_counties.topojson"));
 
-    // helps to load the data asynchronously
+    // Promise helps to load the data asynchronously
     // bind the output into callback function
     Promise.all(promises).then(callback);
 
@@ -68,7 +70,7 @@ function setMap(){
         setEnumerationUnits(wisconsinCounties, map, path, colorScale)
 
         //add coordinated visualization to the map
-        setChart(csvData, colorScale);
+        setDotPlot(csvData, colorScale);
        
     };
 };
@@ -196,7 +198,7 @@ function setChart(csvData, colorScale){
     //create a scale to size bars proportionally to frame and for axis
     var yScale = d3.scaleLinear()
         .range([463, 0])
-        .domain([0, 100]);
+        .domain([0, 0.6]);
 
     //set bars for each province
     var bars = chart.selectAll(".bar")
@@ -228,7 +230,105 @@ function setChart(csvData, colorScale){
         .attr("x", 40)
         .attr("y", 40)
         .attr("class", "chartTitle")
-        .text("Number of Variable " + expressed + " in each region");
+        .text( arrayDict[expressed]);
+
+    //create vertical axis generator
+    var yAxis = d3.axisLeft()
+        .scale(yScale);
+
+    //place axis
+    var axis = chart.append("g")
+        .attr("class", "axis")
+        .attr("transform", translate)
+        .call(yAxis);
+
+    //create frame for chart border
+    var chartFrame = chart.append("rect")
+        .attr("class", "chartFrame")
+        .attr("width", chartInnerWidth)
+        .attr("height", chartInnerHeight)
+        .attr("transform", translate);
+};
+
+//function to create coordinated lollipop chart
+// source URL - https://d3-graph-gallery.com/graph/lollipop_basic.html
+function setDotPlot(csvData, colorScale){
+    // create chart dimensions
+    var chartWidth = window.innerWidth * 0.425,
+        chartHeight = 473,
+        leftPadding = 25,
+        rightPadding = 2,
+        topBottomPadding = 5,
+        chartInnerWidth = chartWidth - leftPadding - rightPadding,
+        chartInnerHeight = chartHeight - topBottomPadding * 2,
+        translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
+
+    //create a second svg element to hold the bar chart
+    var chart = d3.select("body")
+        .append("svg")
+        .attr("width", chartWidth)
+        .attr("height", chartHeight)
+        .attr("class", "chart");
+
+     //create a rectangle for chart background fill
+     var chartBackground = chart.append("rect")
+     .attr("class", "chartBackground")
+     .attr("width", chartInnerWidth)
+     .attr("height", chartInnerHeight)
+     .attr("transform", translate);
+
+     //create a scale for the x-axis
+    var xScale = d3.scalePoint()
+        .range([0, chartInnerWidth])
+        .domain(csvData.map(function(d) { return d.NAMELSAD; }))
+        .padding(1);
+
+    //create a scale to size bars proportionally to frame and for axis
+    var yScale = d3.scaleLinear()
+        .range([463, 0])
+        .domain([0, 0.6]);
+
+    // set lines for each province
+    var myline = chart.selectAll(".myline")
+        .data(csvData)
+        .enter()
+        .append("rect")
+        .attr("class", function(d){
+            return "line " + d.NAMELSAD;
+        })
+        .attr("width", "0.5")
+        .attr("x", function(d, i){
+            return xScale(d.NAMELSAD) + leftPadding;
+        })
+        .attr("y", function(d, i){
+            return yScale(parseFloat(d[expressed])) + topBottomPadding;
+        })
+        .attr("height", function(d, i){
+            return 463 - yScale(parseFloat(d[expressed]));
+        });
+
+     // circles
+     var mycircle = chart.selectAll(".mycircle")
+        .data(csvData)
+        .join("circle")
+        .attr("cx", function(d){
+            return xScale(d.NAMELSAD) + leftPadding;
+        })
+        .attr("cy", function(d){
+            return yScale(parseFloat(d[expressed])) + topBottomPadding;
+        })
+        .attr("r", "4")
+        .style("fill", function(d){
+            return colorScale(d[expressed])
+        })
+        .attr("stroke", "#636363");
+
+    //create a text element for the chart title
+    var chartTitle = chart.append("text")
+        .attr("x", 40)
+        .attr("y", 40)
+        .attr("class", "chartTitle")
+        .text( arrayDict[expressed]);
 
     //create vertical axis generator
     var yAxis = d3.axisLeft()
